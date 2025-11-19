686dd8d069c788fa6add8f124839a6ed5513d804
8209250620sunjiaxuan <bdlfy_0902@qq.com>
2025-11-19 22:52:52 +0800

    Add files via upload

---
+++ 圆锥计算.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#define PI 3.1415926;
+int main() {
+	double r;
+	cin >> r;
+	double h;
+	cin >> h;
+	double V=pow(r, 2) * h * 1.0 / 3 * PI;
+	cout << V;
+}
